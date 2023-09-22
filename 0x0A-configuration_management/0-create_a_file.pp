b304a4af94ad2dd3d49333cc0f945ef91b5436ce
olaosebikan pelumi <ooloasebikan350@gmail.com>
2023-09-22 15:47:57 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # this is  creating a file
 file { '/tmp/school':
-ensure => 'file',
-content => 'I love Puppet',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
+ ensure => 'file',
+ content => 'I love Puppet',
+ mode => '0744',
+ owner => 'www-data',
+ group => 'www-data',
 }
