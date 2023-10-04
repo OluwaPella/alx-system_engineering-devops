671ce173aafccd0daa023dca3c302109657a22d8
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-04 11:49:14 +0100

    very good

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # this script install using puppet
-file { '~/.ssh/id_rsa':
-  ensure => present
+file {' ~/.ssh/id_rsa':
+  ensure  => file,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600';
