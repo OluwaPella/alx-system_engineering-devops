9c97db02f385a5fba9bfa54c28b41d5f4db4a3a1
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-05 23:41:25 +0100

    very good

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
+packege{ 'nginx';
+  ensure => installed,
+}
 
+file_line { 'install';
+  ensure => 'present',
+  path   => '/etc/nginx/sites_enabled/default',
+  after  =>  'listen 80 default_server',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/ permanent',
+}
+
+file { '/var/www/html/index.html',
+  content => 'Hello Word!',
+}
+
+server { 'nginx';
+  ensure  => running,
+  require => package['nginx'],
+}
