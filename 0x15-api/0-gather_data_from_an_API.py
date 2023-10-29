859f048ce943344ab555e41175d5017873422abc
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-29 19:32:03 +0100

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This use REST API   
+# its display on the standard output the employee todo list progress
+import requests
+
+def get_employee_todo_progress(employee_id):
+    # sending a GET request to the api endpoint 
+    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userid={employee_id}")
+    if response.status_code == 200:
+        todos = response.json()
+    compelet_task = [todo for todo in todos if todo['completed']]
+    num_compelet_task = len(compelet_task)
+    total_task = len(todos)
+    employee_name = todos[0]['username']
+    print(f"Employee {employee_name} is done with tasks({num_compelet_task}/{total_task}):")
+
+    for task in compelet_task:
+        print(f"\t{task['title']}")
+    else:
+        print(f"Error:failed to retrieve TODO list for {employee_id}.")
+
+        if __name__ == "__main__":)
