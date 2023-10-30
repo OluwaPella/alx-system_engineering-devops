63434357f97645f089bc6ea9f2eecdeda756a508
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-30 13:22:26 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,7 +12,7 @@ def get_employee_todo_progress(employee_id):
     compelet_task = [todo for todo in todos if todo['completed']]
     NUMBER_OF_DONE = len(compelet_task)
     TOTAL_NUMBER_OF_TASKS = len(todos)
-    EMPLOYEE_NAME = todos[0].get['username']
+    EMPLOYEE_NAME = todos[0]['username']
     print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE}/{TOTAL_NUMBER_OF_TASKS}):")
 
     for task in compelet_task:
