dbc2f54cbace5dcc9e3d947c4e0fdb220df3a922
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-30 15:55:46 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,11 +4,12 @@
 """this module uses http request"""
 import requests
 import sys
-if  __name__ == "__main__":
+
+if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
     todos = requests.get(url + 'todos',  params={"userId": sys.argv[1]}).json()
     completed = [todo for todo in todos if todo.get("completed") is True]
-    print('Employee {} is done with tasks({}/{}):'.format
-            (user.get("name"), len(completed), len(todos)))
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user.get("name"), len(completed), len(todos)))
     [print('\t {}'.format(c.get("title"))) for c in completed]
