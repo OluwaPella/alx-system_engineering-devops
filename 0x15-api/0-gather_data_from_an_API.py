c5c97a08e3053811d563eb9f22584bff6383be0b
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-30 14:35:28 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,18 +6,18 @@ import requests
 
 def get_employee_todo_progress(employee_id):
     # sending a GET request to the api endpoint 
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userid={employee_id}")
+    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
     if response.status_code == 200:
         todos = response.json()
     compelet_task = [todo for todo in todos if todo['completed']]
-    NUMBER_OF_DONE = len(completede)
+    NUMBER_OF_DONE = len(compelet_task)
     TOTAL_NUMBER_OF_TASKS = len(todos)
     EMPLOYEE_NAME = todos[0]['username']
     print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE}/{TOTAL_NUMBER_OF_TASKS}):")
 
     for task in compelet_task:
-        print(f"\t{task[' TASK_TITLE']}")
+        print(f"\t{task['TASK_TITLE']}")
 
-    if  __name__ == "__main__":
-        employee_id = 1  # Set the employee ID
-        get_employee_todo_progress(employee_id)
+if  __name__ == "__main__":
+    employee_id = 1  # Set the employee ID
+    get_employee_todo_progress(employee_id)
