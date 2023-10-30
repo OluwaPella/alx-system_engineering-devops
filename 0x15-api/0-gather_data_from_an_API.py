7cc91dc039ff67a5d54a1bcdf6f040e132069798
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-30 14:27:52 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,13 +12,11 @@ def get_employee_todo_progress(employee_id):
     compelet_task = [todo for todo in todos if todo['completed']]
     NUMBER_OF_DONE = len(completede)
     TOTAL_NUMBER_OF_TASKS = len(todos)
-    EMPLOYEE_NAME = todos[0]['username]
+    EMPLOYEE_NAME = todos[0]['username']
     print(f"Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE}/{TOTAL_NUMBER_OF_TASKS}):")
 
     for task in compelet_task:
         print(f"\t{task[' TASK_TITLE']}")
-    else:
-        print(f"Error:failed to retrieve TODO list for {employee_id}.")
 
     if  __name__ == "__main__":
         employee_id = 1  # Set the employee ID
