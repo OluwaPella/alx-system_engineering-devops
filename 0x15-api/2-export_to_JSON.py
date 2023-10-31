40ec5066d404ef1467aa553d37c387d98b682b63
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-11-01 00:23:29 +0100

    api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exporting todo list information data in CSV format"""
+"""Exporting todo list information data in json format"""
 import json
 import requests
 import sys
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # Specify the CSV file name based on user ID
     with open("{}.json".format(user_id), 'w') as jsonfile:
         for todo in todos:
-            json.dump({user_id, [{
+            json.dump({user_id: [{
                 "task": todo.get("title"),
                 "completed": todo.get("completed"),
                 "username": user.get("username")
