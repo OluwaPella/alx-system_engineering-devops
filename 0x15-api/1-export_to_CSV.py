353253072a6042f8c67d48a87af20bda78c95f76
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-31 14:31:35 +0100

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  exporting todo list information  data in  CSV format"""
+"""Exporting todo list information data in CSV format"""
 import requests
 import csv
 import sys
@@ -7,14 +7,32 @@ import sys
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     user_id = sys.argv[1]
-    user = requests.get(url + 'user{}'.format(user_id)).json()
+    
+    # Fetch user data
+    user = requests.get(url + 'users/{}'.format(user_id)).json()
+    
+    # Fetch todos for the user
     todos = requests.get(url + 'todos', params={"userId": user_id}).json()
-    completed = (todo for todo in todos if todo.get("completed") is True)
+    
+    # Filter completed todos
+    completed = [todo for todo in todos if todo.get("completed")]
+    
+    # Specify the CSV file name based on user ID
     csv_file = '{}.csv'.format(user_id)
-    with open(csv_file, 'w' newline='') as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-            (writer.writerow[
-                [user_id,
-                user, todo.get("completed"),
-                todo.get("title")
-                ]) for todo in todos]
+    
+    # Open CSV file for writing
+    with open(csv_file, 'w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        
+        # Write the CSV header row
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        
+        # Write data for each todo
+        for todo in todos:
+            writer.writerow([
+                user_id,
+                user['username'],  # Extract the 'username' attribute from user data
+                "Completed" if todo["completed"] else "Not Completed",
+                todo["title"]
+            ])
+ 
