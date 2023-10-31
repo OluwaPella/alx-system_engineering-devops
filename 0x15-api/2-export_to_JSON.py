fe3ca9681d19aa0c167d71f9d493bdf9991aee8d
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-31 17:35:52 +0100

    api

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Exporting todo list information data in CSV format"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_id = sys.argv[1]
+    # Fetch user data
+    user = requests.get(url + 'users/{}'.format(user_id)).json()
+    # Fetch todos for the user
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    # Specify the CSV file name based on user ID
+    with open("{}.json".format(user_id), 'w', newline='') as file:
+        writer = json.writer(file, quoting=csv.QUOTE_ALL)
+        # Write the CSV header row
+        for todo in todos:
+            writer.writerow([
+                user_id,
+                user.get("username"),
+                todo.get("completed"),
+                todo.get("title")
+                ])
