74f4245100e7b1befc4dbf23a3a9f4c08f4cd685
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-31 15:51:39 +0100

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,9 +17,9 @@ if __name__ == "__main__":
     with open("{}.csv".format(user_id), 'w', newline='') as csvfile:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         # Write the CSV header row
-            (writer.writerow[
+            writer.writerow([
                 [user_id,
                 user.get["username"],
                 "Completed" if t.get["completed"] else "Not Completed",
                 t.get["title"]
-            ]) for todo in todos]
+            ]) for todo in todos
