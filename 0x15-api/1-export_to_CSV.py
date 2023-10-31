a1f328c477d2a4cc204152e11c02dcd5ec3aad9b
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-31 15:33:44 +0100

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         for todo in todos:
             writer.writerow([
                 user_id,
-                user['username'],  # Extract the 'username' attribute from user data
+                user['username'],
                 "Completed" if todo["completed"] else "Not Completed",
                 todo["title"]
             ])
