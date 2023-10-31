d446afd05a6201960cca118d64b271865f3a0259
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-10-31 15:01:39 +0100

    api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -22,8 +22,7 @@ if __name__ == "__main__":
         for todo in todos:
             writer.writerow([
                 user_id,
-                username,
-                todo.get("completed"),
-                todo.get("title")
+                "username": user.get("username"),
+                "completed": todo.get("completed"),
+                "tasks": todo.get("title")
             ])
- 
