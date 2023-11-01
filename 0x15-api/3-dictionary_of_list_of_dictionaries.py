e549c528542022f2bfee2bcc99629ae91ef5db7b
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-11-01 02:41:18 +0100

    api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,12 +7,13 @@ import sys
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     # Fetch user data
-    user = requests.get(url + 'users/{}').json()
-    # Fetch todos for the user
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    user = requests.get(url + 'users').json()
     # Specify the CSV file name based on user ID
     with open("todo_all_employees.json", 'w') as jsonfile:
-        json.dump({: [{todo.get("user")
+        json.dump({user.get("id"): [{todo.get("user")
             "task": todo.get("title"),
             "completed": todo.get("completed"),
-            "username": user.get("username")} for todo in todos]}, jsonfile)
+            "username": user.get("username")
+        } for todo in requests.get(url + "todos", 
+                params={"userId": user.get("id")}).json()
+             for user in users]}, jsonfile)
