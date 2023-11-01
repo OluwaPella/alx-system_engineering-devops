45af2e9499d2b1173db83b016b903858afd60475
olaosebikan pelumi <oolaosebikan350@gmail.com>
2023-11-01 02:51:43 +0100

    api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,7 +2,6 @@
 """Exporting todo list information data in json format"""
 import json
 import requests
-import sys
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
@@ -10,10 +9,11 @@ if __name__ == "__main__":
     user = requests.get(url + 'users').json()
     # Specify the CSV file name based on user ID
     with open("todo_all_employees.json", 'w') as jsonfile:
-        json.dump({user.get("id"): [{todo.get("user")
-            "task": todo.get("title"),
-            "completed": todo.get("completed"),
-            "username": user.get("username")
-        } for todo in requests.get(url + "todos", 
+        json.dump({
+            user.get("id"): [{
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username")
+            } for todo in requests.get(url + "todos", 
                 params={"userId": user.get("id")}).json()]
         for user in users}, jsonfile)
